4b6f49a7ae591627198d0fa80673f95deff46fe6
karthikraja <karthikraja.sr@gmail.com>
2009-11-07 02:26:43 +0800

    UI change - to get video to play in the same page

--- app/controllers/stage_controller.rb
+++ app/controllers/stage_controller.rb
@@ -1,41 +1,46 @@
 class StageController < ApplicationController
   def new
-  @stage = Stage.new
-  @user=User.find(:all)
-end
-
-def create
-  #@user=User.find(:all, :conditions => {:id => params[:id]}, :order => 'created_at DESC')
-  @stage = Stage.new(params[:stage])
-  @stage.user_id=session[:user_id]
-  
-  if @stage.save
-    flash[:notice] = 'File was successfully created.'
-    redirect_to :action=>'index',:id=>session[:user_id]
-  else
-    flash[:notice] = 'File creation was unsuccessful.'
-    render :action => :new
+    @stage = Stage.new
+    @user=User.find(:all)
   end
-end
 
-def index
-   @stages = Stage.find(:all, :conditions => {:user_id => session[:user_id]}, :order => 'created_at DESC')
-   @user_id=params[:id]
-   respond_to do |format|
+  def create
+    #@user=User.find(:all, :conditions => {:id => params[:id]}, :order => 'created_at DESC')
+    @stage = Stage.new(params[:stage])
+    @stage.user_id=session[:user_id]
+    
+    if @stage.save
+      flash[:notice] = 'File was successfully created.'
+      redirect_to :action=>'index',:id=>session[:user_id]
+    else
+      flash[:notice] = 'File was unsuccessfully created.'
+      render :action => :new
+    end
+  end
+
+  def index
+    @stages = Stage.find(:all, :conditions => {:user_id => session[:user_id]}, :order => 'created_at DESC')
+    @user_id=params[:id]
+    respond_to do |format|
       format.html # index.rhtml
       format.xml  { render :xml => @stages.to_xml }
     end
 
-end
+  end
 
-def show
-  @file= '/public'+params[:file]
-  @target_file=@file.gsub('.3gp','.flv')
-  @location=@target_file.gsub('/public/swf/','')
-  @location.to_s.strip
-  logger.debug "currently trying to convert the video ( #{@file} ) path ( #{@file}) from content type of #{@target_file} to flv"
-    system("ffmpeg.exe -i #{RAILS_ROOT + @file} -ar 22050 -ab 32 -s 480x360 -acodec libmp3lame -vcodec flv -r 25 -qscale 8 -b 25000000 -acodec libmp3lame -f flv -y #{ RAILS_ROOT+@target_file }")
-end
+  def show
+      @stages = Stage.find(:all, :conditions => {:user_id => session[:user_id]}, :order => 'created_at DESC')
+      @file= '/public'+params[:file]
+      @target_file=@file.gsub('.3gp','.flv')
+      @location=@target_file.gsub('/public/swf/','')
+      @location.to_s.strip
+      @filename=params[:name]
+      logger.debug "currently trying to convert the video ( #{@file} ) path ( #{RAILS_ROOT}) from content type of #{@target_file} to flv"
+      system("ffmpeg -i #{RAILS_ROOT + @file} -ar 44100  -ac 2 -vol 1024 -ab 64 -s 480x360 -acodec libmp3lame -vcodec flv -r 25 -qscale 8 -b 25000000 -acodec libmp3lame -f flv -y #{ RAILS_ROOT+@target_file }")
+      
+
+  
+  end
 
 
 
