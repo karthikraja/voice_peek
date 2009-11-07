ee6dd99863040a96b9eee57563df7d9d35a4af3a
karthikraja <karthikraja.sr@gmail.com>
2009-11-08 03:03:54 +0800

    minor change in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,17 @@
-# Settings specified here will take precedence over those in config/environment.rb
+# since you don't have to restart the webserver when you make code changes.
+config.cache_classes = false
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+#config.action_view.cache_template_extensions         = false
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 config.action_controller.asset_host                  = "174.129.39.130/"
