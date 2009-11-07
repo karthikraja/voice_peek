88c6920e00337977a2ae5e6d07db41d1dcc663f0
karthikraja <karthikraja.sr@gmail.com>
2009-11-07 22:55:32 +0800

    new production server update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,9 @@ config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-config.action_controller.asset_host                  = "75.101.147.214/"
+config.action_controller.asset_host                  = "174.129.39.130/"
 
-SITE="75.101.147.214"
+SITE="174.129.39.130"
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
